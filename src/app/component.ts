9a1da4cb9e0bb7390ba7fd4cc8bc6afc14e54c83
devcom2020 <kwp@tlen.pl>
2022-09-27 11:18:34 +0200

    wtorek 11:18

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { ApplicationRef, Component } from "@angular/core";
+import { Product } from "./product.model";
 import { Model } from "./repository.model";
 
 @Component({
@@ -8,32 +9,21 @@ import { Model } from "./repository.model";
 export class ProductComponent {
   model: Model = new Model();
 
-  // getClasses(): string{
-  //   return this.model.getProducts().length == 5 ? "bg-success": "bg-warning";
-  // }
-
-  getClasses(key: number): string {
-    let product = this.model.getProduct(key);
-    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+  constructor(ref: ApplicationRef) {
+    (<any>window).appRef = ref;
+    (<any>window).model = this.model;
   }
-
-  getClassMap(key:number): Object {
-    let product = this.model.getProduct(key);
-    return {
-      "text-center bg-danger": product.name == "Kajak",
-      "bg-info": product.price < 50
-    };
+  getProductByPosition(position: number): Product {
+    return this.model.getProducts()[position];
   }
-
-  getStyles(key:number){
-    let product = this.model.getProduct(key);
-    return{
-      fontSize: "30px",
-      "margin.px": 100,
-      color: product.price > 50? "red": "green"
-    };
+  getProduct(key: number): Product {
+    return this.model.getProduct(key);
   }
-
-  fontSizeWithUnits: string = "30px";
-  fontSizeWithoutUnits: string = "30";
+  getProducts(): Product[] {
+    return this.model.getProducts();
+  }
+  getProductCount(): number {
+    return this.getProducts().length;
+  }
+  targetName: string = "Kajak";
 }
