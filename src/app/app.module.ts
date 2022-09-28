9bb7db1a7edb499c418e9be8656aedee33f96fe6
devcom2020 <kwp@tlen.pl>
2022-09-28 10:00:47 +0200

    sroda 10:00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
 import { PaAttrDirective } from './attr.directive';
+import { PaModel } from './twoway.directive';
 
 @NgModule({
-  declarations: [ProductComponent, PaAttrDirective],
+  declarations: [ProductComponent, PaAttrDirective, PaModel],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule], providers: [],
   bootstrap: [ProductComponent]
 })
